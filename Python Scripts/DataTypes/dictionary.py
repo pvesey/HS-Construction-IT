721f3ca3cc7e956e2af5df8f20ce765e68998624
Paul Vesey <pvesey@users.noreply.github.com>
2020-02-08 20:38:04 +0000

    DataType Update

--- Python Scripts/DataTypes/dictionary.py
+++ Python Scripts/DataTypes/dictionary.py
@@ -1,1 +1,11 @@
 # dictionary.py
+
+# key value pair
+
+my_dictionary = {"Alice": "085-123-4567", "Bob": "086-234-5678", "Eve": "087-345-6789"}
+
+print(my_dictionary["Bob"])
+
+my_dictionary_tuple = {"Chuck":("Effort", 20, 34.2) , "Dan":("Control", 26, 76.1) }
+
+print(my_dictionary_tuple["Chuck"][1])
--- Python Scripts/DataTypes/tuple.py
+++ Python Scripts/DataTypes/tuple.py
@@ -1,1 +1,18 @@
-# tuple.py 
+# tuple.py
+
+# tuple for immutable data
+days = ("Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday")
+
+# tuple can hold different variable types
+mixed_types = ("Pipe", 100, "Galv Steel", 102.5)
+
+print(days[3])
+print(mixed_types[1], type(mixed_types[1]))
+
+
+# Combining lists and tuples for more use cases
+
+my_tuple_list = [("Alice", 53), ("Bob", 31), ("Eve", 32)]
+
+print(my_tuple_list[0])
+print(my_tuple_list[0][0])
